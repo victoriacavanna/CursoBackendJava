f3df5d006e597ec3ee1ac1555d7f83e1429363c7
Victoria Cavanna <viccavanna@gmail.com>
2023-06-03 19:50:15 -0300

    Ejercicio 14

---
+++ ejercicio14.java
@@ -0,0 +1,34 @@
+package ejercicio14;
+import java.util.Scanner;
+
+public class Ejercicio14 {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        double euros;
+        String divisa;
+        System.out.println("Ingrese el monto de euros a convertir: ");
+        euros = read.nextDouble();
+        
+        read.nextLine();
+        
+        System.out.println("Ingrese a que moneda desea convertir: dolares, yenes o libras");
+        divisa = read.nextLine().toLowerCase();
+        convertir(euros, divisa);
+    }
+    public static void convertir(double cantidad, String moneda){
+        double conversion;
+        if (moneda.equals("libras")){
+            conversion = cantidad * 0.86;
+        } else if (moneda.equals("dolares")){
+            conversion = cantidad * 1.28611;
+        } else if (moneda.equals("yenes")){
+            conversion = cantidad * 129.852;
+        } else {
+            conversion = 0;
+        }
+        
+        System.out.println(cantidad + "3€ = " + conversion +" "+ moneda);
+    }
+
+}
