4f10df342c39dfb092c1ce777817cb4c3943d1e6
Victoria Cavanna <viccavanna@gmail.com>
2023-06-03 19:44:10 -0300

    Ejercicio 9

---
+++ ejercicio9.java
@@ -0,0 +1,25 @@
+package ejercicio9;
+import java.util.Scanner;
+
+
+public class Ejercicio9 {
+
+    public static void main(String[] args) {
+        boolean validacionA;
+        Scanner read = new Scanner(System.in);
+        
+        System.out.println("Ingrese una frase o palabra que empiece con la letra A: ");
+        String texto = read.nextLine();
+        
+        validacionA = texto.startsWith("A") || texto.startsWith("a") ;
+        
+        if (validacionA){
+            System.out.println("CORRECTO");
+        } else {
+            System.out.println("INCORRECTO");
+        }
+        
+        
+    }
+
+}
