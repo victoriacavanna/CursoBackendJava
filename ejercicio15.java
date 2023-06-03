f610219e4285acb14205465306562822df498d24
Victoria Cavanna <viccavanna@gmail.com>
2023-06-03 19:51:03 -0300

    Ejercicio 15

---
+++ ejercicio15.java
@@ -0,0 +1,19 @@
+package ejercicio15;
+import java.util.Scanner;
+
+public class Ejercicio15 {
+
+    public static void main(String[] args) {
+        int [] vector = new int [100];
+        llenarVector(vector);
+        for (int i = 99; i >= 0; i--){
+            System.out.println(vector[i]);
+        }
+    }
+    public static void llenarVector(int[]arreglo){
+        for (int i = 0; i < 100; i++){
+            arreglo[i] = i + 1;
+        }
+    }
+
+}
