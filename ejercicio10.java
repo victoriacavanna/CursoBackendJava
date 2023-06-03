9d3a755ed8878e26e6652432c06e08d7d1ed2e63
Victoria Cavanna <viccavanna@gmail.com>
2023-06-03 19:44:57 -0300

    Ejercicio 10

---
+++ ejercicio10.java
@@ -0,0 +1,28 @@
+package ejercicio10;
+import java.util.Scanner;
+
+public class Ejercicio10 {
+
+    public static void main(String[] args) {
+        int limite;
+        double suma;
+        double num;
+        
+        Scanner read = new Scanner(System.in);
+        
+        do {
+            System.out.println("Ingrese valor límite positivo: ");
+            limite = read.nextInt();
+        } while (limite <= 0);
+        
+        suma = 0;
+        while (suma < limite){
+            System.out.println("Ingrese un número: ");
+            num = read.nextInt();
+            suma += num;
+        }
+        
+        System.out.println( suma + " supera el límite de "+ limite);
+    }
+
+}
