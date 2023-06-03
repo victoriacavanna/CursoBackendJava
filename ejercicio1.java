9bc0e2b935e09a8ec87ee959973d163b986e582d
Victoria Cavanna <viccavanna@gmail.com>
2023-06-03 19:34:44 -0300

    Ejercicio 1

---
+++ ejercicio1.java
@@ -0,0 +1,26 @@
+package ejercicio1; 
+import java.util.Scanner;
+
+
+public class Ejercicio1 {
+    
+    public static void main(String[] args) {
+        int resultadoSuma;
+        Scanner read = new Scanner(System.in);
+        
+        System.out.println("Ingrese un número: ");
+        int num1 = read.nextInt();
+        
+         System.out.println("Ingrese otro número: ");
+        int num2 = read.nextInt();
+        
+        resultadoSuma = Sumar(num1, num2);
+        
+        System.out.println(num1 + " + " + num2 + " = " + resultadoSuma);
+    }
+    
+    public static int Sumar(int numA, int numB){
+        int result = numA + numB;
+        return result;
+    }
+}
